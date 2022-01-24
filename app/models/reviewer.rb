10e49d740dbf69e7271758ab07c721498ebf7b05
Chris Fung <aergonaut@gmail.com>
2022-01-24 10:07:32 -0800

    fix(slack): remove slack ping (#933)

--- app/models/reviewer.rb
+++ app/models/reviewer.rb
@@ -15,8 +15,6 @@ class Reviewer < ApplicationRecord
 
   has_paper_trail
 
-  after_save :send_outbound_notifications, if: -> { saved_change_to_login? }
-
   def addendum
     <<~ADDENDUM
       ### #{name_with_code}
@@ -59,46 +57,6 @@ class Reviewer < ApplicationRecord
     self.status = STATUS_APPROVED
     self.approved_at = Time.now.utc
     save!
-  end
-
-  def send_outbound_notifications
-    if (user = User.find_by(login: login))
-      send_slack_message(recipient: user)
-    end
-  end
-
-  def send_slack_message(recipient:)
-    return unless recipient.slack_identity
-
-    text =
-      <<~MESSAGE
-        You were assigned a new code review. View the Pull Request below.
-      MESSAGE
-
-    pr_html_url = pull_request.html_url
-    attachments = [
-      {
-        fallback: "View the Pull Request at #{pr_html_url}",
-        title: pull_request.full_title,
-        title_link: pr_html_url,
-        fields: [
-          {
-            title: "Review Context",
-            value: review_rule&.name || "Peer Review",
-            short: true
-          }
-        ],
-        actions: [
-          {
-            type: "button",
-            text: "View Pull Request",
-            url: pr_html_url
-          }
-        ]
-      }
-    ]
-
-    SendSlackMessage.perform_async(recipient.id, text, attachments)
   end
 
   private
